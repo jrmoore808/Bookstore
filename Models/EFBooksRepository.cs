99fde92c6ea1f9857bf443cada85b874d8f0128a
jrmoore808 <61597022+jrmoore808@users.noreply.github.com>
2021-02-17 10:19:52 -0700

    Add project files.

---
+++ Models/EFBooksRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class EFBooksRepository : IBooksRepository
+    {
+        private BooksDbContext _context;
+        public EFBooksRepository (BooksDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Books> Books => _context.Books;
+    }
+}
